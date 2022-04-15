65732b0ca12777485ff13aa46793aa039b518cfb
tu-leminh <mt.the.dev@gmail.com>
2022-04-15 17:00:28 +0700

    init

--- data/get_data.py
+++ data/get_data.py
@@ -8,6 +8,5 @@ driver.get('https://www.worldometers.info/coronavirus/')
 table = driver.find_element_by_id('main_table_countries_today')
 ls = table.find_elements_by_xpath(".//tr")
 df=pd.DataFrame([[(j.text.replace(',','').replace('+','') if j.text!="" else 0) for j in i.find_elements_by_xpath(".//td")][1:-1] for i in ls if i.text!=""][1:-1],columns = ['Country', 'Total Cases', 'New Cases','Total Deaths','New Deaths','Total Recovered','New Recovered','Active Cases','Serious','Tot per 1M','Deaths per 1M','Tests','Tests per 1M','Pop'])
-#save df to data folder
-df.to_csv('data/'+str(datetime.date(datetime.now()))+'.csv',index=False)
+df.to_csv('data/'+str(datetime.date(datetime.now()))+'.csv')
 driver.quit()
